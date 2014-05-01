1a6a74f2a3b8b062fac05622120ff9041e048b51
Richard Rothwell <farad@farad.com.au>
2014-05-01 14:33:55 +1000

    RR: core-site.xml configuration

--- install_master_node.sh
+++ install_master_node.sh
@@ -16,6 +16,10 @@
 # Ignore "start failed: message.
 apt-get -y install hadoop-0.20-mapreduce-jobtracker hadoop-hdfs-namenode hadoop-client
 
+# -------------------------------------------
+# Storage Management
+# -------------------------------------------
+
 # Upload and run on ALL VM's.
 # This is mainly for the master. 
 # Maybe not necessary to apply the entire script to the slaves as some of the 
@@ -24,4 +28,30 @@ apt-get -y install hadoop-0.20-mapreduce-jobtracker hadoop-hdfs-namenode hadoop-
 # MUST be executed in isolation.
 ./bind_hadoop_directories.sh
 
+# -------------------------------------------
+# Install HDFS
+# -------------------------------------------
 
+# It looks like we can just set up the config files on the master
+# and then copy the master's config directory to all the slaves.
+# Not sure how far to go with this; does it include the secondary namenode as well?
+
+# Now configure our cluster. On all machines in cluster.
+cp -r /etc/hadoop/conf.empty /etc/hadoop/conf.unicarbkb
+update-alternatives --install /etc/hadoop/conf hadoop-conf /etc/hadoop/conf.unicarbkb 50
+update-alternatives --set hadoop-conf /etc/hadoop/conf.unicarbkb
+
+# # Edit core-site.xml on master, slave 1, slave 2, slave 3 etc..
+# Its easier to edit everything on the master and when finished 
+# copy the entire config directory to the other cluster members.
+
+cat <<DELIMITER > /etc/hadoop/conf.unicarbkb/core-site.xml
+<?xml version="1.0"?>
+<?xml-stylesheet type="text/xsl" href="configuration.xsl"?>
+<configuration>
+    <property>
+        <name>fs.defaultFS</name>
+        <value>hdfs://$hadoop_master_domain/</value>
+    </property>
+</configuration>
+DELIMITER
