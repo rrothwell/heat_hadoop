0893e9b7dab96d86236b8e7b18a6ea4e66328683
Richard Rothwell <farad@farad.com.au>
2014-05-01 12:43:06 +1000

    RR: debugging startup issue.

--- install_master_node.sh
+++ install_master_node.sh
@@ -14,6 +14,6 @@
 # Master: JobTracker host and  NameNode host
 # Probably should have separate VM's for jobtracker and namenode.
 # Ignore "start failed: message.
-#apt-get install hadoop-0.20-mapreduce-jobtracker hadoop-hdfs-namenode hadoop-client
+apt-get -y install hadoop-0.20-mapreduce-jobtracker hadoop-hdfs-namenode hadoop-client
 
 
