96111e02760d04706ec519eace4151137d50f5b4
Richard Rothwell <farad@farad.com.au>
2014-05-01 11:57:02 +1000

    RR: debug startup failure.

--- install_common.sh
+++ install_common.sh
@@ -40,10 +40,10 @@ echo -e "$hadoop_master_ip\t$hadoop_master_domain\t$hadoop_master_name" >> /etc/
 # http://www.cloudera.com/content/cloudera-content/cloudera-docs/CDH4/4.2.0/CDH4-Installation-Guide/cdh4ig_topic_4_4.html
 
 # reference the "1 click" package. Do this for ALL machines.
-wget http://archive.cloudera.com/cdh4/one-click-install/precise/amd64/cdh4-repository_1.0_all.deb
-dpkg -i cdh4-repository_1.0_all.deb
+#wget http://archive.cloudera.com/cdh4/one-click-install/precise/amd64/cdh4-repository_1.0_all.deb
+#dpkg -i cdh4-repository_1.0_all.deb
 # Install the repo key.
-wget -qO - http://archive.cloudera.com/cdh4/ubuntu/precise/amd64/cdh/archive.key | apt-key add -
-apt-get update; 
+#wget -qO - http://archive.cloudera.com/cdh4/ubuntu/precise/amd64/cdh/archive.key | apt-key add -
+#apt-get update
 
 
