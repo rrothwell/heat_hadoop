cf128647ea13c2351bc3759fcca9ca2cc214e153
Richard Rothwell <farad@farad.com.au>
2014-05-08 14:18:17 +1000

    RR: debugging finalisation process.

--- finalise_auxiliary.sh
+++ finalise_auxiliary.sh
@@ -6,10 +6,7 @@
 # Adjust configuration of remote VM's and then starts the services
 # ===========================================
 
-remote_password="0^3Dfxx"
-auxilary_node_ip=130.56.249.85
-
-sudo update-rc.d hadoop-hdfs-secondarynamenode defaults
+update-rc.d hadoop-hdfs-secondarynamenode defaults
 service hadoop-hdfs-secondarynamenode start
 
 
