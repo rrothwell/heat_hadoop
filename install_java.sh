3b8b3028b4fd42d5a4eb7101f1cc77fb44ab765b
Richard Rothwell <rrothwell@v3.org.au>
2014-12-16 14:45:34 +1100

    RR: update Java install to favour OpenJDK over Oracle.

--- install_java.sh
+++ install_java.sh
@@ -15,30 +15,44 @@
 # and limitations under the License.
 # ===========================================
 # install_java.sh
+# Environment: Ubuntu: 12.04
 # ===========================================
-# Install Java on ALL machines.
 # Oracle Java version 7
+# Oracle Java download can become excruciatingly slow, hence favour openjdk-7-jdk
 
-# See: http://www.cloudera.com/content/cloudera-content/cloudera-docs/CDH4/4.2.0/CDH4-Installation-Guide/cdh4ig_topic_29.html
-# And: http://www.cloudera.com/content/cloudera-content/cloudera-docs/CDH4/4.2.0/CDH4-Installation-Guide/cdh4ig_topic_29_1.html?scroll=topic_29_1
-# But as far as I can tell the instructions there are not necessary as far as setting
-# JAVA_HOME and PATH are concerned.
+# Ensure JAVA_HOME and PATH are setup?
+
+# Resources:
+#	https://www.digitalocean.com/community/tutorials/how-to-install-java-on-ubuntu-with-apt-get
 # ===========================================
 
-#Register repo
-add-apt-repository -y ppa:webupd8team/java
-apt-get -q -y update 
+JDK=
+#JDK=Oracle
 
-# Bypass Oracle license dialog.
-echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
-echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
+if [ "$JDK" == "Oracle" ]; then
+	#Register repo
+	add-apt-repository -y ppa:webupd8team/java
+	apt-get -q -y update 
 
-# Install.
-apt-get -y install java-common  oracle-java7-installer  oracle-java7-set-default
+	# Bypass Oracle license dialog.
+	echo debconf shared/accepted-oracle-license-v1-1 select true | debconf-set-selections
+	echo debconf shared/accepted-oracle-license-v1-1 seen true | debconf-set-selections
 
-# Register alternatives. Maybe some redundancy here that can be removed later.
-update-java-alternatives -s java-7-oracle
+	# Install.
+	apt-get -y install java-common  oracle-java7-installer  oracle-java7-set-default
 
-# Set the JAVA environment variables.
-echo -e "\n\nJAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /etc/environment;
-export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
+	# Register alternatives. Maybe some redundancy here that can be removed later.
+	update-java-alternatives -s java-7-oracle
+
+	# Set the JAVA environment variables.
+	echo -e "\n\nJAVA_HOME=/usr/lib/jvm/java-7-oracle" >> /etc/environment;
+	export JAVA_HOME=/usr/lib/jvm/java-7-oracle/
+	
+else
+	apt-get install -y openjdk-7-jdk
+
+	# Set the JAVA environment variables.
+	echo -e "\n\nJAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64" >> /etc/environment;
+	export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64/
+fi
+
